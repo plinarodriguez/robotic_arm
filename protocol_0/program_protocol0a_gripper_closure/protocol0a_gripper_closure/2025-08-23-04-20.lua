2f6a7f22b523e7d176af1a5e801d3860aa2edafd
Paulina <52298128+paulina-rodriguez@users.noreply.github.com>
2025-08-22 17:09:39 -0600

    finished all single bottle protocols with new fingers

--- protocol_0/program_protocol0a_gripper_closure/protocol0a_gripper_closure/2025-08-23-04-20.lua
+++ protocol_0/program_protocol0a_gripper_closure/protocol0a_gripper_closure/2025-08-23-04-20.lua
@@ -5,7 +5,6 @@ ActGripper(1,1)
 MoveGripper(1,75,75,17,5000,0,0,0,0,0)
 MoveGripper(1,50,75,17,5000,0,0,0,0,0)
 MoveGripper(1,25,75,17,5000,0,0,0,0,0)
-MoveGripper(1,0,75,17,5000,0,0,0,0,0)
 MoveGripper(1,100,75,17,5000,0,0,0,0,0)
 ActGripper(1,0)
 loop1 = loop1 + 1
