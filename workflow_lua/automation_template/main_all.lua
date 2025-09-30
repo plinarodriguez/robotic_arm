f092c97a4f9fc5a278ede6afda18afc2ee5cf16d
Paulina <52298128+paulina-rodriguez@users.noreply.github.com>
2025-09-30 12:37:43 -0600

    updates

---
+++ workflow_lua/automation_template/main_all.lua
@@ -0,0 +1,33 @@
+bottleStartxyz = {
+        x = 101.498, 
+        y = -288.711,
+        z = 135.0, 
+        rx = -179.124,
+        ry = 78.208,
+        rz = 90.446,
+    }
+bottle_params{
+        tool = 0,
+        user = 0,
+        speed = 20.0,
+        acc = 20.0,
+        ovl = 1.0,
+        ep1 = 0,ep2 = 0,ep3 = 0,ep4 = 0,
+        blendR = -1.0, -- I had 0 before but this might be safer: -1 = for no blending
+        search = 0,
+        offset = 0,
+        offset_x = 0,offset_y = 0,offset_z = 0,
+        offset_rx = 0,offset_ry = 0,offset_rz = 0,
+    }
+bottle_approach_z = 135.0,
+    bottle_grab_z = 67.883,
+    d_x = 0.5,      -- space between bottles
+    d_x_last = 0.5, -- space between bottles at the end of row 
+    d_y_last = 0.5, -- space between bottles at the end of row
+    filler_wait = 3000,
+    cols = 6,
+    rows = 8,
+    bottle_params = -1,
+
+--------
+
